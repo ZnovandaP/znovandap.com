8168e500911b51293957b7648c5bb5b4c3bef18b
ZnovandaP <znovandaputra@gmail.com>
2023-12-12 14:52:10 +0700

    [create] data constant icons and data constant projects

---
+++ src/constant/data-projects/index.ts
@@ -0,0 +1,43 @@
+const dataProjects = [
+  {
+    title: 'znovandap.com',
+    to: '/',
+    theme: '',
+    description: '',
+    summary: 'This website contains detailed information about me as a front-end web developer.',
+    image: {
+      thumbnail: '/assets_porto/znovandadotcom/znovandadotcom.png',
+      primary: '/assets_porto/znovandadotcom/home.svg',
+      responsive: '/assets_porto/znovandadotcom/responsive.svg',
+    },
+    stacks: ['TypeScript', 'NEXT.JS', 'Zustand', 'TailwindCSS', 'SWR'],
+  },
+  {
+    title: 'Mushaf App',
+    to: '/',
+    theme: '#00AD7C',
+    description: '',
+    summary: 'The web application like other Muslim apps, provides Al-Quran include vocal audio, daily prayers, etc.',
+    image: {
+      thumbnail: '/assets_porto/mushaf_app/mushaf.png',
+      primary: '/assets_porto/mushaf_app/home.svg',
+      responsive: '/assets_porto/mushaf_app/responsive.svg',
+    },
+    stacks: ['TypeScript', 'React', 'Vite', 'PWA', 'MUI', 'ReactQuery'],
+  },
+  {
+    title: 'RakBukuKu',
+    to: '/',
+    theme: '#FD4242',
+    description: '',
+    summary: 'The bookshelf web application, implement CRUD for data manipulate, the data saved on local storage (web API).',
+    image: {
+      thumbnail: '/assets_porto/rakbukuku/rakbukuku.png',
+      primary: '/assets_porto/rakbukuku/home.svg',
+      responsive: '/assets_porto/rakbukuku/responsive.svg',
+    },
+    stacks: ['JavaScript', 'React', 'Redux', 'Vite', 'PWA', 'TailwindCSS'],
+  },
+];
+
+export default dataProjects;
---
+++ src/constant/icons/index.tsx
@@ -0,0 +1,66 @@
+import * as React from 'react';
+
+import {
+  SiCss3,
+  SiFramer,
+  SiGithub,
+  SiLinkedin,
+  SiInstagram,
+  SiGit,
+  SiAxios,
+  SiBootstrap,
+  SiWebpack,
+  SiHtml5,
+  SiJavascript,
+  SiJest,
+  SiJasmine,
+  SiCodeceptjs,
+  SiPwa,
+  SiMui,
+  SiNextdotjs,
+  SiReact,
+  SiReactquery,
+  SiReactrouter,
+  SiRedux,
+  SiSass,
+  SiTailwindcss,
+  SiTypescript,
+  SiVite,
+  SiSwr,
+} from 'react-icons/si';
+
+type StacksProps = {
+  [key: string]: React.JSX.Element | string
+};
+
+const Icons: StacksProps = {
+  Instagram: <SiInstagram className="text-[#E4405F]" />,
+  LinkedIn: <SiLinkedin />,
+  Github: <SiGithub />,
+  Git: <SiGit className="text-[#F05032]" />,
+  JavaScript: <SiJavascript className="text-[#F7DF1E]" />,
+  TypeScript: <SiTypescript className="text-[#3178C6]" />,
+  HTML: <SiHtml5 className="text-[#E34F26]" />,
+  CSS: <SiCss3 className="text-[#1572B6]" />,
+  Saas: <SiSass className="text-[#CC6699]" />,
+  Bootstrap: <SiBootstrap className="text-[#7952B3]" />,
+  TailwindCSS: <SiTailwindcss className="text-[#06B6D4]" />,
+  React: <SiReact className="text-[#61DAFB]" />,
+  'NEXT.JS': <SiNextdotjs />,
+  ReactRouter: <SiReactrouter className="text-[#CA4245]" />,
+  ReactQuery: <SiReactquery className="text-[#FF4154]" />,
+  Axios: <SiAxios className="text-[#5A29E4]" />,
+  SWR: <SiSwr />,
+  MUI: <SiMui className="text-[#007FFF]" />,
+  FramerMotion: <SiFramer />,
+  Vite: <SiVite className="text-[#646CFF]" />,
+  Webpack: <SiWebpack className="text-[#8DD6F9]" />,
+  PWA: <SiPwa className="text-[#5A0FC8]" />,
+  Jest: <SiJest className="text-[#C21325]" />,
+  Jasmine: <SiJasmine className="text-[#8A4182]" />,
+  CodeceptJs: <SiCodeceptjs className="text-[#F6E05E]" />,
+  Redux: <SiRedux className="text-[#764ABC]" />,
+  Zustand: '🐻',
+};
+
+export default Icons;
