3c9bfe17f8ee06ca32786e9b5b5a0ca0167236d0
ZnovandaP <znovandaputra@gmail.com>
2023-12-12 15:07:53 +0700

    [update] adding section Last Projects

--- src/views/home/HomeView.tsx
+++ src/views/home/HomeView.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
 import Container from '@/components/Container';
+import Line from '@/components/Decoration/Line';
 import Prologue from './layout/Prologue';
+import LastProjects from './layout/LastProjects';
 
 export default function HomeView() {
   return (
     <Container className="sm:mt-[8rem]">
       <Prologue />
+
+      <Line size="sm" className="m-8" borderStyle="border-solid" />
+      <LastProjects />
+
     </Container>
   );
 }
