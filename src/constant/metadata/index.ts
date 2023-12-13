0bf7fcd2787d1b0b647235d97f747fa8119ad688
ZnovandaP <znovandaputra@gmail.com>
2023-12-14 00:50:18 +0700

    [create] tempalate metadata

---
+++ src/constant/metadata/index.ts
@@ -0,0 +1,18 @@
+const METADATA = {
+  creator: 'Zidane Novanda Putra',
+  description: 'Personal website, website portofolio, portofolio, znovandap blog, znovadap front-end web developer',
+  keyword: 'znovandap, znovandap personal website, znovandap portofolio, znovandap blog, Zidane Novada Putra, front-end web developer, front-end engineer',
+  authors: {
+    name: 'Zidane Novanda Putra',
+    url: process.env.DOMAIN,
+  },
+  openGraph: {
+    url: process.env.DOMAIN,
+    siteName: 'znovandap',
+    locale: 'id-ID',
+  },
+  exTitle: '| znovandap',
+  profile: '/author_fill.webp',
+};
+
+export default METADATA;
