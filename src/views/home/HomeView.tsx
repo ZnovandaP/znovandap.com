eb8529ff1e54c0b3f2b6fe9fec7e90ccfef42fec
ZnovandaP <znovandaputra@gmail.com>
2023-12-14 00:55:03 +0700

    [update] amout component section Latest Blogs

--- src/views/home/HomeView.tsx
+++ src/views/home/HomeView.tsx
@@ -1,17 +1,15 @@
 import * as React from 'react';
 import Container from '@/components/Container';
-import Line from '@/components/Decoration/Line';
 import Prologue from './layout/Prologue';
 import LastProjects from './layout/LastProjects';
+import LatestBlogs from './layout/LatestBlogs';
 
 export default function HomeView() {
   return (
     <Container className="sm:mt-[8rem]">
       <Prologue />
-
-      <Line size="sm" className="m-8" borderStyle="border-solid" />
       <LastProjects />
-
+      <LatestBlogs />
     </Container>
   );
 }
