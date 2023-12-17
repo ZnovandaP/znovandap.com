7ce609e5085ede9d0d96a3df64151d860f5e4571
ZnovandaP <znovandaputra@gmail.com>
2023-12-17 10:37:44 +0700

    [create] utility function for sorting blog post by date

---
+++ src/libs/utils/sortPostBlogyDate.ts
@@ -0,0 +1,24 @@
+import type { MetadataBlog } from '@/types/mdx';
+import loadMdXFile from '../markdown';
+
+export const latestPostBlogs = () => {
+  const getAllPostBlogs = loadMdXFile('blogs');
+
+  return getAllPostBlogs.sort((post1, post2) => {
+    const data1 = post1.frontMatter as MetadataBlog;
+    const data2 = post2.frontMatter as MetadataBlog;
+
+    return new Date(data2.date).getTime() - new Date(data1.date).getTime();
+  });
+};
+
+export const newestPostBlog = () => {
+  const getAllPostBlogs = loadMdXFile('blogs');
+
+  return getAllPostBlogs.sort((post1, post2) => {
+    const data1 = post1.frontMatter as MetadataBlog;
+    const data2 = post2.frontMatter as MetadataBlog;
+
+    return new Date(data1.date).getTime() - new Date(data2.date).getTime();
+  });
+};
