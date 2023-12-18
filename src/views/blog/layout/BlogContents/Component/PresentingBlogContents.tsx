42bd24ac976e36ebf0665d7194d9d2bcc43a1eb8
ZnovandaP <znovandaputra@gmail.com>
2023-12-19 01:38:18 +0700

    [feature] adding feature skeleton loading when waiting response blog post

--- src/views/blog/layout/BlogContents/Component/PresentingBlogContents.tsx
+++ src/views/blog/layout/BlogContents/Component/PresentingBlogContents.tsx
@@ -6,6 +6,8 @@ import CardBlog from '@/components/Card/CardBlog';
 import CardContainer from '@/views/common/CardContainer';
 import getDataBlogPostSortBy from '@/service/blog-post-fetcher';
 import CardListBlog from '@/components/Card/CardListBlog';
+import SkeletonCardBlog from '@/components/Skeleton/SkeletonCardBlog';
+import SkeletonCardListBlog from '@/components/Skeleton/SkeletonCardListBlog';
 
 type PresentingBlogContentsProps = {
   sortByDate: string
@@ -27,9 +29,21 @@ export default function PresentingBlogContents(
 
   return (
     <section>
-      {isLoading && (
-        <p className="text-2xl font-semibold">Loading...</p>
-      )}
+      {isLoading && currentLayout === 'grid' ? (
+        <CardContainer>
+          {Array.from(new Array(6).keys()).map((key) => (
+            <SkeletonCardBlog key={key} />
+          ))}
+        </CardContainer>
+      ) : null}
+
+      {isLoading && currentLayout === 'list' ? (
+        <div className="grid grid-cols-1 gap-8 mt-8">
+          {Array.from(new Array(6).keys()).map((key) => (
+            <SkeletonCardListBlog key={key} />
+          ))}
+        </div>
+      ) : null}
 
       {currentLayout === 'grid' && (
         <CardContainer>
@@ -50,7 +64,6 @@ export default function PresentingBlogContents(
           ))}
         </div>
       )}
-
     </section>
   );
 }
