f4c5c6bb7dc3c8574d63f488efac5abf72dc84d2
ZnovandaP <znovandaputra@gmail.com>
2023-12-18 22:23:12 +0700

    [update] adding new props (className props)

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,13 +1,19 @@
 import * as React from 'react';
 import Image from 'next/image';
+import cn from '@/libs/utils/cn';
 
 type CardProps = {
   children: React.ReactNode
+  className?: string
 };
-export default function Card({ children }: CardProps) {
+export default function Card({ children, className = '' }: CardProps) {
   return (
     <div
-      className="bg-stone-200 dark:bg-stone-800 shadow-lg shadow-stone-500/60 ring-2 ring-stone-500/60 rounded-lg overflow-hidden hover:scale-[1.025] active:scale-100 transition-all duration-300"
+      className={cn(
+        'bg-stone-200 dark:bg-stone-800 shadow-lg shadow-stone-500/60 ring-2 ring-stone-500/60 rounded-lg overflow-hidden hover:scale-[1.025] active:scale-100 transition-all duration-300',
+
+        className,
+      )}
     >
       {children}
     </div>
@@ -16,7 +22,7 @@ export default function Card({ children }: CardProps) {
 
 function CardMedia({ src, alt }: { src: string, alt: string }) {
   return (
-    <div className="w-full h-[250px]">
+    <div className="w-full h-[200px]">
       <Image
         src={src}
         alt={alt}
@@ -29,13 +35,29 @@ function CardMedia({ src, alt }: { src: string, alt: string }) {
   );
 }
 
-function CardContent({ children }: CardProps) {
+function CardListMedia({ src, alt }: { src: string, alt: string }) {
   return (
-    <div className="px-6 pt-4 pb-6">
+    <div className="center py-6 pl-6">
+      <Image
+        src={src}
+        alt={alt}
+        width={600}
+        height={600}
+        loading="lazy"
+        className="w-[260px] h-[150px] rounded-md"
+      />
+    </div>
+  );
+}
+
+function CardContent({ children, className = '' }: CardProps) {
+  return (
+    <div className={`px-6 pt-4 pb-6 ${className}`}>
       {children}
     </div>
   );
 }
 
+Card.CardListMedia = CardListMedia;
 Card.CardMedia = CardMedia;
 Card.CardContent = CardContent;
