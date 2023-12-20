55d193b575d6b9496a6a47307b6e945343883ab5
ZnovandaP <znovandaputra@gmail.com>
2023-12-20 20:44:39 +0700

    [feature] adding metadata for best experience and improve SEO on blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,16 @@
 import * as React from 'react';
+import { Metadata } from 'next';
+import METADATA from '@/constant/metadata';
 import BlogViews from '@/views/blog';
+
+export const metadata: Metadata = {
+  title: `Blog ${METADATA.exTitle}`,
+  description: 'My blogs content sharing about programming specially front-end web development and tech',
+  keywords: 'blog znovandap, znovandap',
+  alternates: {
+    canonical: `${process.env.DOMAIN}/blog`,
+  },
+};
 
 export default function BlogPage() {
   return (
--- src/app/blog/post/[slug]/page.tsx
+++ src/app/blog/post/[slug]/page.tsx
@@ -1,7 +1,37 @@
 import * as React from 'react';
 import BlogPostViews from '@/views/blog/slug';
+import { Metadata } from 'next';
+import loadMdXFile from '@/libs/markdown';
+import { MdxFileProps } from '@/types/mdx';
+import METADATA from '@/constant/metadata';
 
-export default function BlogPostPage({ params }: { params: { slug: string } }) {
+type BlogPostPageProps = {
+  params: { slug: string }
+};
+
+export async function generateMetadata({ params }: BlogPostPageProps): Promise<Metadata> {
+  const blog = await loadMdXFile()
+    .find((post) => post.slug === params.slug) as MdxFileProps;
+
+  return {
+    title: `${blog.frontMatter.title} ${METADATA.exTitle}`,
+    description: blog.frontMatter.subtitle,
+    openGraph: {
+      images: blog.frontMatter.image,
+      url: `${process.env.DOMAIN}/${blog.slug}`,
+      siteName: METADATA.openGraph.siteName,
+      locale: METADATA.openGraph.locale,
+      type: 'article',
+      authors: 'Zidane Novanda Putra',
+    },
+    keywords: blog.frontMatter.title,
+    alternates: {
+      canonical: `${process.env.DOMAIN}/blog/post/${blog.slug}`,
+    },
+  };
+}
+
+export default function BlogPostPage({ params }: BlogPostPageProps) {
   return (
     <BlogPostViews slug={params.slug} />
   );
