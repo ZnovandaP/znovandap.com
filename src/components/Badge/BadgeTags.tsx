a1b3e23775bcc8f1864899f7d6dab978665cfc9e
ZnovandaP <znovandaputra@gmail.com>
2023-12-20 16:50:55 +0700

    [refactor] component BadgeTag can use in other size

--- src/components/Badge/BadgeTags.tsx
+++ src/components/Badge/BadgeTags.tsx
@@ -1,15 +1,32 @@
-import Icons from '@/constant/icons';
 import * as React from 'react';
+import Icons from '@/constant/icons';
+import cn from '@/libs/utils/cn';
 
 type BadgeTagsProps = {
   stack: string
+  size?: 'sm' | 'md'
+  className?: string
 };
 
-export default function BadgeTags({ stack }: BadgeTagsProps) {
+export default function BadgeTags({ stack, size = 'sm', className }: BadgeTagsProps) {
   return (
-    <div className="center gap-2 px-2 py-1 ring-1 ring-purple-500 rounded-full text-xs font-medium">
+    <div className={cn(
+      'center gap-2 ring-1 ring-stone-500 rounded-full font-medium',
+
+      size === 'sm' && 'text-xs px-2 py-1',
+
+      size === 'md' && 'text-base px-3 py-2',
+
+      className,
+    )}
+    >
       {Icons[stack] && (
-      <span className="text-sm">
+      <span className={cn(
+        size === 'sm' && 'text-sm',
+
+        size === 'md' && 'text-xl',
+      )}
+      >
         {Icons[stack]}
       </span>
       )}
