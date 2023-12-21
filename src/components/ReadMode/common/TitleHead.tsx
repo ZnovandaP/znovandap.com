a1b6170462194178308e725b2118130da59e4e59
ZnovandaP <znovandaputra@gmail.com>
2023-12-21 23:42:47 +0700

    [update] adding new props theme for coloring text title in component TitleHead.tsx

--- src/components/ReadMode/common/TitleHead.tsx
+++ src/components/ReadMode/common/TitleHead.tsx
@@ -3,12 +3,16 @@ import * as React from 'react';
 type TitleHeadProps = {
   title: string
   subtitle: string
+  theme?: string
 };
 
-export default function TitleHead({ subtitle, title }: TitleHeadProps) {
+export default function TitleHead({ subtitle, title, theme = '' }: TitleHeadProps) {
   return (
     <div className="flex flex-col gap-3">
-      <h1 className="flex items-center gap-2 text-balance text-3xl font-bold sm:text-4xl lg:text-5xl">
+      <h1
+        style={{ color: theme }}
+        className="flex items-center gap-2 text-balance text-3xl font-bold sm:text-4xl lg:text-5xl"
+      >
         {title}
       </h1>
       <h3 className="text-lg md:text-xl font-medium opacity-80">
