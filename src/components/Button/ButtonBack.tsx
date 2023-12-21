c80ab06e1a97e517169b503cd6ff89994b7fe204
ZnovandaP <znovandaputra@gmail.com>
2023-12-21 20:59:21 +0700

    [update] re-styling button back component

--- src/components/Button/ButtonBack.tsx
+++ src/components/Button/ButtonBack.tsx
@@ -7,16 +7,17 @@ import Link from 'next/link';
 type ButtonBackProps = {
   className: string
   to: string
+  text?: string
 };
 
-export default function ButtonBack({ className = '', to }: ButtonBackProps) {
+export default function ButtonBack({ className = '', to, text = '' }: ButtonBackProps) {
   return (
     <Link
       href={to}
-      className={`center gap-2 rounded-lg px-3 py-2 ring-1 ring-stone-500/60 hover:scale-110 active:scale-100 transition duration-300 w-32 font-medium ${className}`}
+      className={`center gap-2 rounded-lg px-3 py-2 ring-1 ring-stone-500/60 hover:scale-110 active:scale-100 transition duration-300 font-medium ${className}`}
     >
       <IoMdArrowBack className="text-xl" />
-      Back
+      {text || 'back'}
     </Link>
   );
 }
