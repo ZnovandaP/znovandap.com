e528334559f2ab86c92bd6eb7f5a0db8b18ed074
ZnovandaP <znovandaputra@gmail.com>
2023-12-22 16:55:01 +0700

    [bugfix] fixing bug data(frontMatter projects) sorting by date is latest update

--- src/views/home/layout/LatestProjects/CardProjects.tsx
+++ src/views/home/layout/LatestProjects/CardProjects.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import { MetadataProject } from '@/types/mdx';
-import { oldestPostProjects } from '@/libs/utils/sortingPostProjectByDate';
+import { latestPostProjects } from '@/libs/utils/sortingPostProjectByDate';
 import CardProject from '@/components/Card/CardProject';
 import CardContainer from '../../../common/CardContainer';
 
 export default function CardProjects() {
-  const latestProjects = oldestPostProjects();
+  const latestProjects = latestPostProjects();
   return (
     <CardContainer>
       {latestProjects.slice(0, 3).map((data) => {
