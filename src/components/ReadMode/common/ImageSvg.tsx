86c2bcfeeae22fb2e8815a3a1c485bf728f2cf6d
ZnovandaP <znovandaputra@gmail.com>
2023-12-23 04:13:27 +0700

    [update] re-styling Component ImageSvg for ImageHeadSvg in Page body

--- src/components/ReadMode/common/ImageSvg.tsx
+++ src/components/ReadMode/common/ImageSvg.tsx
@@ -8,14 +8,14 @@ type ImageHeadSvgProps = {
 
 export default function ImageHeadSvg({ src, alt }: ImageHeadSvgProps) {
   return (
-    <div className="center rounded-lg overflow-hidden w-full h-[10rem] sm:h-[13rem] md:h-[20rem] lg:h-[22rem]">
+    <div className="center rounded-lg overflow-hidden w-full h-[10rem] sm:h-[15rem] md:h-[22rem] lg:h-[25rem]">
       <Image
         src={src}
         alt={alt}
         width={800}
         height={800}
         priority
-        className="w-full h-full rounded-lg hover:scale-110 transition duration-300"
+        className="w-[950px] h-full rounded-lg hover:scale-110 transition duration-300"
       />
     </div>
   );
