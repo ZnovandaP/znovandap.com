ce274cf380fcc0d20c5c7bbf9f04f0d11dee73ed
ZnovandaP <znovandaputra@gmail.com>
2023-12-25 15:07:38 +0700

    [update] add metadata on project page and project slug page

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,9 +1,38 @@
-import DetailProjectViews from '@/views/projects/slug';
 import * as React from 'react';
+import { Metadata } from 'next';
+import METADATA from '@/constant/metadata';
+import loadMdXFile from '@/libs/markdown';
+import { MdxFileProps, MetadataProject } from '@/types/mdx';
+import DetailProjectViews from '@/views/projects/slug';
 
 type DetailProjectsPageProps = {
   params: { slug: string }
 };
+
+export async function generateMetadata({ params }: DetailProjectsPageProps): Promise<Metadata> {
+  const project = await loadMdXFile('projects')
+    .find((post) => post.slug === params.slug) as MdxFileProps;
+
+  const frontMatter = project.frontMatter as MetadataProject;
+
+  return {
+    title: `${frontMatter.title} ${METADATA.exTitle}`,
+    description: frontMatter.subtitle,
+    openGraph: {
+      images: frontMatter.thumbnail,
+      url: `${process.env.DOMAIN}/${project.slug}`,
+      siteName: METADATA.openGraph.siteName,
+      locale: METADATA.openGraph.locale,
+      type: 'article',
+      authors: 'Zidane Novanda Putra',
+    },
+    keywords: frontMatter.title,
+    alternates: {
+      canonical: `${process.env.DOMAIN}/projects/${project.slug}`,
+    },
+  };
+}
+
 export default function DetailProjectsPage({ params }: DetailProjectsPageProps) {
   return (
     <DetailProjectViews slug={params.slug} />
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,17 @@
 import * as React from 'react';
+import { Metadata } from 'next';
 import ProjectsView from '@/views/projects';
+import METADATA from '@/constant/metadata';
 import { latestPostProjects, oldestPostProjects } from '@/libs/utils/sortingPostProjectByDate';
+
+export const metadata: Metadata = {
+  title: `Projects ${METADATA.exTitle}`,
+  description: 'My Projects content sharing about my journey learning programming specially front-end web development',
+  keywords: 'projects znovandap, znovandap projects, zidane novanda putra projects',
+  alternates: {
+    canonical: `${process.env.DOMAIN}/projects`,
+  },
+};
 
 const dataSort = {
   oldest: oldestPostProjects(),
