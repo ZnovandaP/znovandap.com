29073fa100725cebeb5e3dfb4feac2427719dcab
ZnovandaP <znovandaputra@gmail.com>
2023-12-25 23:48:39 +0700

    [create] data for link on contact page

---
+++ src/constant/data-link-contact/index.ts
@@ -0,0 +1,38 @@
+import {
+  SiGithub, SiGmail, SiInstagram, SiLinkedin, SiThreads,
+} from 'react-icons/si';
+
+const dataLinkContact = [
+  {
+    title: 'Github',
+    color: '#181717',
+    href: 'https://github.com/ZnovandaP/',
+    Icon: SiGithub,
+  },
+  {
+    title: 'LinkedIn',
+    color: '#0A66C2',
+    href: 'https://www.linkedin.com/in/zidane-novanda-putra/',
+    Icon: SiLinkedin,
+  },
+  {
+    title: 'Instagram',
+    color: '#E4405F',
+    href: 'https://www.instagram.com/znovanda_p/',
+    Icon: SiInstagram,
+  },
+  {
+    title: 'Threads',
+    color: '#000',
+    href: 'https://www.threads.net/@znovanda_p',
+    Icon: SiThreads,
+  },
+  {
+    title: 'Gmail',
+    color: '#EA4335',
+    href: 'mailto:znovandaputra@gmail.com',
+    Icon: SiGmail,
+  },
+];
+
+export default dataLinkContact;
