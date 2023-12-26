b8a35014cf55d065ec8748c60cb3cdc3e3154aab
ZnovandaP <znovandaputra@gmail.com>
2023-12-26 21:27:31 +0700

    [update] amount component ContactView on ContactPage and add Metadata

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,19 @@
 import * as React from 'react';
+import { Metadata } from 'next';
+import METADATA from '@/constant/metadata';
+import ContactView from '@/views/contact';
+
+export const metadata: Metadata = {
+  title: `Projects ${METADATA.exTitle}`,
+  description: 'My Projects content sharing about my journey learning programming specially front-end web development',
+  keywords: 'projects znovandap, znovandap projects, zidane novanda putra projects',
+  alternates: {
+    canonical: `${process.env.DOMAIN}/projects`,
+  },
+};
 
 export default function ContactPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Contact Page
-    </main>
+    <ContactView />
   );
 }
