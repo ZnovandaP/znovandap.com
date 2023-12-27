59c2a9ece234aed11152c6ea3486f806a6842746
ZnovandaP <znovandaputra@gmail.com>
2023-12-27 07:23:05 +0700

    [update] add new props on component common view PrologueSection

--- src/views/common/PrologueSection.tsx
+++ src/views/common/PrologueSection.tsx
@@ -1,16 +1,26 @@
 import * as React from 'react';
 import TitleHead from '@/components/Typography/TitleHead';
+import cn from '@/libs/utils/cn';
 
 type PrologueSectionProps = {
   title: string
   icon?: React.ReactElement
   paragraph: string
+  type?: 'head-section' | 'head-page'
 };
-export default function PrologueSection({ title, icon, paragraph }: PrologueSectionProps) {
+export default function PrologueSection({
+  title, icon, paragraph, type = 'head-page',
+}: PrologueSectionProps) {
   return (
     <>
       <TitleHead text={title} icon={icon} />
-      <p className="mt-4 font-medium">
+      <p className={cn(
+        'mt-4',
+        type === 'head-page' && 'text-lg opacity-75 font-semibold',
+
+        type === 'head-section' && 'font-medium',
+      )}
+      >
         {paragraph}
       </p>
     </>
