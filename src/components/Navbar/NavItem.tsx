7a50633fc0f8732e1ab22a318aa568f425eee278
ZnovandaP <znovandaputra@gmail.com>
2023-12-27 23:13:39 +0700

    [bugfix] fixing feature navlink not work in dynamic page (nesting page)

--- src/components/Navbar/NavItem.tsx
+++ src/components/Navbar/NavItem.tsx
@@ -14,6 +14,8 @@ type NavItemProps = {
 
 export default function NavItem({ data, column, row }: NavItemProps) {
   const pathname = usePathname();
+  const arrOfRoute = pathname.split('/');
+  const baseRoute = `/${arrOfRoute[1]}`;
   return (
     <li>
       <Link
@@ -26,7 +28,7 @@ export default function NavItem({ data, column, row }: NavItemProps) {
 
                 row && 'flex gap-2 items-center py-2 px-4 rounded-full',
 
-                pathname === data.pathname
+                baseRoute === data.pathname
                 && 'font-bold bg-stone-200/5 backdrop-blur-md shadow-md shadow-stone-500/60 dark:shadow-stone-800',
               )
             }
