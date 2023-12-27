d44a91daa43ee40dd18a0b1d3690e7eccdb606d5
ZnovandaP <znovandaputra@gmail.com>
2023-12-28 00:01:54 +0700

    [update] amount component AboutValue on Page.tsx (about page)

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,19 @@
 import * as React from 'react';
+import { Metadata } from 'next';
+import METADATA from '@/constant/metadata';
+import AboutView from '@/views/about';
+
+export const metadata: Metadata = {
+  title: `About ${METADATA.exTitle}`,
+  description: 'About page on znovandap.com, contains my biodata and my journy why i\'am interest to become a front-end web developer',
+  keywords: 'about znovandap, znovandap about, zidane novanda putra about, about zidane novanda putra',
+  alternates: {
+    canonical: `${process.env.DOMAIN}/about`,
+  },
+};
 
 export default function AboutPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      About Page
-    </main>
+    <AboutView />
   );
 }
