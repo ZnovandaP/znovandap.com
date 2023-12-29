4dafc15780e8f0ca9361d049e5aef2c4135ffa7c
ZnovandaP <znovandaputra@gmail.com>
2023-12-30 01:27:57 +0700

    [create] Component Main

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+
+'use client';
+
+import * as React from 'react';
+import useOpenNavStore from '@/libs/zustand/nav-menu-store';
+
+type MainProps = {
+  children: React.ReactNode
+};
+export default function Main({ children }: MainProps) {
+  const closeNavMenu = useOpenNavStore((state) => state.setClose);
+
+  const handleClickMain = () => {
+    closeNavMenu();
+  };
+
+  return (
+    <main onClick={handleClickMain}>
+      {children}
+    </main>
+  );
+}
