94fb479046dbee82ffff75f51f9052576fbbd315
ZnovandaP <znovandaputra@gmail.com>
2023-12-29 23:12:17 +0700

    [update] amount component EntertaimentView in EntertsimentPages

--- src/app/entertaiment/page.tsx
+++ src/app/entertaiment/page.tsx
@@ -1,9 +1,21 @@
 import * as React from 'react';
+import { Metadata } from 'next';
+import METADATA from '@/constant/metadata';
+import EntertaimentView from '@/views/entertaiment';
 
-export default function EntertaimentPage() {
+export const metadata: Metadata = {
+  title: `Entertaiment ${METADATA.exTitle}`,
+  description: 'Entertaiment page on znovandap.com, on this page contains a collection if memes or jokes in Bahasa, this page just for fun',
+  keywords: 'jokes/memes znovandap, znovandap entertaiment',
+  alternates: {
+    canonical: `${process.env.DOMAIN}/entertaiment`,
+  },
+};
+
+export default function EntertaimentPage(
+  { searchParams }: { searchParams: { [key: string]: string | string[] | undefined } },
+) {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Entertaiment Page
-    </main>
+    <EntertaimentView searchParams={searchParams.page as string} />
   );
 }
