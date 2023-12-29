cd347f9c54cd3600b42876b1f920a1a783402e3a
ZnovandaP <znovandaputra@gmail.com>
2023-12-29 23:02:09 +0700

    [update] adding hostname/domain for configure next/image

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [
+      'i.pinimg.com',
+      'awsimages.detik.net.id',
+      'cdn.idntimes.com',
+      'cdn0-production-images-kly.akamaized.net',
+      'cdn1-production-images-kly.akamaized.net',
+      'cdn-image.hipwee.com',
+      'media.suara.com',
+      'cdn-brilio-net.akamaized.net',
+      'akcdn.detik.net.id',
+      'cdns.klimg.com',
+      'cdns.idntimes.com',
+      's3.theasianparent.com',
+      'www.brilio.net',
+      'media-assets-ggwp.s3.ap-southeast-1.amazonaws.com',
+    ],
+  },
+}
 
 module.exports = nextConfig
