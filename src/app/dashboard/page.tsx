8933a616047b3a37bb363cce2397f432cdd81807
ZnovandaP <znovandaputra@gmail.com>
2023-12-30 18:20:42 +0700

    [update] remove route to DashboardPage

--- src/app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-export default function DashboardPage() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Dashboard Page
-    </main>
-  );
-}
