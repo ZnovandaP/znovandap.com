eadf6cd644d3ba42501c3bd4b465dc5d2e01683c
ZnovandaP <znovandaputra@gmail.com>
2024-01-22 09:07:32 +0700

    [refactor] on files CardProjects and CardBlogs in feature LatestProjects and LatestBlogs on home page

--- src/views/home/layout/LatestBlogs/CardBlogs.tsx
+++ src/views/home/layout/LatestBlogs/CardBlogs.tsx
@@ -1,15 +1,19 @@
 import * as React from 'react';
 import CardBlog from '@/components/Card/CardBlog';
-import { latestPostBlogs } from '@/libs/utils/sortPostBlogyDate';
+import { latestBlogPosts } from '@/libs/utils/sortPostBlogyDate';
 import { MetadataBlog } from '@/types/mdx';
 import CardContainer from '../../../common/CardContainer';
 
 export default function CardBlogs() {
-  const latestPosts = latestPostBlogs().slice(0, 3);
+  const latestPosts = latestBlogPosts().slice(0, 3);
   return (
     <CardContainer>
       {latestPosts.map((post) => (
-        <CardBlog data={post.frontMatter as MetadataBlog} slug={post.slug} key={post.slug} />
+        <CardBlog
+          data={post.frontMatter as MetadataBlog}
+          slug={post.slug}
+          key={post.id}
+        />
       ))}
     </CardContainer>
   );
--- src/views/home/layout/LatestProjects/CardProjects.tsx
+++ src/views/home/layout/LatestProjects/CardProjects.tsx
@@ -1,20 +1,20 @@
 import * as React from 'react';
 import { MetadataProject } from '@/types/mdx';
-import { latestPostProjects } from '@/libs/utils/sortingPostProjectByDate';
+import { latestProjectPosts } from '@/libs/utils/sortingPostProjectByDate';
 import CardProject from '@/components/Card/CardProject';
 import CardContainer from '../../../common/CardContainer';
 
 export default function CardProjects() {
-  const latestProjects = latestPostProjects();
+  const latestProjects = latestProjectPosts();
   return (
     <CardContainer>
-      {latestProjects.slice(0, 3).map((data) => {
-        const frontMatter = data.frontMatter as MetadataProject;
-
-        return (
-          <CardProject data={frontMatter} slug={data.slug} key={data.id} />
-        );
-      })}
+      {latestProjects.slice(0, 3).map((project) => (
+        <CardProject
+          data={project.frontMatter as MetadataProject}
+          slug={project.slug}
+          key={project.id}
+        />
+      ))}
     </CardContainer>
   );
 }
