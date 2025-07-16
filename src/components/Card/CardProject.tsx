6760f5eae2db2a0faf9da9ab774e207067037597
ZnovandaP <znovandaputra@gmail.com>
2025-07-16 10:23:00 +0700

    feat: redesign card for showing content at blogs/projects

--- src/components/Card/CardProject.tsx
+++ src/components/Card/CardProject.tsx
@@ -12,8 +12,8 @@ type CardProjectProps = {
 };
 export default function CardProject({ data, slug }: CardProjectProps) {
   const dateIsoFormat = new Date(data.date).toISOString();
-  const datePostFormat = moment(dateIsoFormat).format('ll');
-  const postTimeAgo = moment(dateIsoFormat).fromNow();
+  const datePostFormat = moment(dateIsoFormat).format('LL');
+  // const postTimeAgo = moment(dateIsoFormat).fromNow();
 
   return (
     <Link href={`projects/${slug}`} data-aos="fade-up">
@@ -22,26 +22,28 @@ export default function CardProject({ data, slug }: CardProjectProps) {
           src={data.thumbnail}
           alt={`Image, Project ${data.title}`}
         />
-        <Card.CardContent className="border-t-2 border-stone-400 dark:border-stone-600">
+        <Card.CardContent className="border-t-2 border-stone-400 dark:border-stone-600 h-[200px] flex flex-col">
           <div className="flex flex-col gap-2">
             <h2
               style={{ color: data.theme }}
-              className="text-xl font-semibold group-hover:underline group-hover:underline-offset-[6px] group-hover:decoration-4 line-clamp-1"
+              className="text-lg font-semibold group-hover:underline group-hover:underline-offset-[6px] group-hover:decoration-4 line-clamp-1"
             >
               {data.title}
             </h2>
-            <p className="opacity-80 text-sm">
-              {`${datePostFormat} - ${postTimeAgo}`}
+            <p className="font-medium text-sm">
+              {`${datePostFormat}`}
             </p>
-            <p className="font-medium hyphens-auto line-clamp-3 lg:line-clamp-2 xl:line-clamp-3">
+            <p className="text-sm text-neutral-400 hyphens-auto line-clamp-3 lg:line-clamp-2 xl:line-clamp-3">
               {data.subtitle}
             </p>
           </div>
 
-          <div className="mt-4 flex items-center gap-4">
+          <div className="mt-auto flex items-center gap-4">
             {data.stacks?.slice(0, 5)?.map((stack) => (
               <Tooltip title={stack} key={stack}>
-                {Icons[stack]}
+                <span className="text-[21px]">
+                  {Icons[stack]}
+                </span>
               </Tooltip>
             ))}
           </div>
