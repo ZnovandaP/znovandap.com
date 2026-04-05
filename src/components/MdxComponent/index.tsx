c3bc88ce8b58c175aea90c13d71133238affdc33
ZnovandaP <znovandaputra@gmail.com>
2026-04-06 01:11:45 +0700

    feat: re-styling component unorder list and order list to markdown

--- src/components/MdxComponent/index.tsx
+++ src/components/MdxComponent/index.tsx
@@ -44,8 +44,8 @@ export default function MdxComponent({ children }: MdxComponentProps) {
         h2: (props) => <h2 className="border-l-[6px] border-sky-500 pl-2 text-2xl mt-6 font-semibold" {...props} />,
         h3: (props) => <h3 className="border-l-4 border-emerald-300/80 pl-2 text-xl mt-6 font-semibold" {...props} />,
         h4: (props) => <h4 className="text-lg mt-6 font-semibold" {...props} />,
-        ul: (props) => <ul className="pl-10 space-y-3 list-disc pb-5 mt-6 font-medium" {...props} />,
-        ol: (props) => <ol className="pl-10 space-y-3 list-decimal pb-5 mt-6 font-medium" {...props} />,
+        ul: (props) => <ul className="pl-10 space-y-3 list-disc pb-3 mt-3 font-medium" {...props} />,
+        ol: (props) => <ol className="pl-10 space-y-3 list-decimal pb-3 mt-3 font-medium" {...props} />,
         blockquote: (props) => (
           <blockquote
             className="pl-6 py-3 border-l-[5px] border-stone-700 border-l-sky-500 bg-stone-100 dark:bg-stone-800 rounded-lg text-sky-800 dark:text-sky-200"
